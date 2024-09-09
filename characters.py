f2f7adb6b90207bf9e3d1e86103621b2d1ef5e34
mkviniciuz <mv.vinicius107@gmail.com>
2024-09-09 15:53:53 -0300

    Update

--- characters.py
+++ characters.py
@@ -4,14 +4,23 @@ class hero:
         self.damage = damage
         self.experience = experience
         self.mana = mana
+        self.maxhealth = health
+        self.bag = []
 
 
     def attack(self, target) -> None:
         target.health -= self.damage
-        print(f"The Hero attacked and deal {self.damage}DMG in the ENEMY!")
-        print(f"Enemy actual life {target.healt}/{max(target.health, 0)}")
-            
-hero1 = hero(10,5,0,0)
-hero2 = hero(15,10,10,10)
+        print(f"The Hero attacked and deal {self.damage} DMG in the ENEMY!")
+        print(f"Enemy actual life {target.health}/{target.maxhealth}")
+    
+    def showBag(self) -> None:
+        if not self.bag:
+            print(f"Your bag is empty for now")
+        else:
+            print("This is your bag actually")
 
-hero1.attack(hero2)
+
+            
+hero1 = hero(10,5,5,0)
+
+hero1.showBag()
