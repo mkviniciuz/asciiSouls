33942432fb2ce172d48e54e0cbb8c8f13965f7ff
mkviniciuz <mv.vinicius107@gmail.com>
2024-09-09 17:44:15 -0300

    att

--- items.py
+++ items.py
@@ -1,3 +1,6 @@
+import characters
+from characters import hero1
+
 #HP Potions functions
 def smallpotion(target):
     target.health += 5
@@ -36,30 +39,19 @@ hpItems = {
 #MP Potions functions
 def smallmppotion(target):
     target.mana += 5
-    if target.mana > target.maxmana:
-        target.mana = target.maxmana
     print(f"You have restored +5 MP")
     print(f"actual MP: {target.mana}/{target.maxmana}")
-
 def mediumpotion(target):
     target.mana += 25
-    if target.mana > target.maxmana:
-        target.mana = target.maxmana
-    print(f"You have restored +25 MP")
+    print(f"You have restored +5 MP")
     print(f"actual MP: {target.mana}/{target.maxmana}")
-
-def largemppotion(target):
+def largepotion(target):
     target.mana += 50
-    if target.mana > target.maxmana:
-        target.mana = target.maxmana
-    print(f"You have restored +50 MP")
+    print(f"You have restored +5 MP")
     print(f"actual MP: {target.mana}/{target.maxmana}")
-
 def hydrapotion(target):
     target.mana += target.maxmana
-    if target.mana > target.maxmana:
-        target.mana = target.maxmana
-    print(f"You have restored MAX MP")
+    print(f"You have restored +5 MP")
     print(f"actual MP: {target.mana}/{target.maxmana}")
 
 mpItems = {
@@ -76,49 +68,4 @@ mpItems = {
     'Hydra MP potion': lambda: hydrapotion(hero),
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class hero:
-    def __init__(self, health, damage, experience, mana) -> None:
-        self.health = health
-        self.damage = damage
-        self.experience = experience
-        self.mana = mana
-        self.maxhealth = health
-        self.maxmana = mana
-        self.bag = []
-
-
-    def attack(self, target) -> None:
-        target.health -= self.damage
-        print(f"The Hero attacked and deal {self.damage} DMG in the ENEMY!")
-        print(f"Enemy actual life {target.health}/{target.maxhealth}")
-    
-    def showBag(self) -> None:
-        if not self.bag:
-            print(f"Your bag is empty for now")
-        else:
-            print("This is your bag actually")
-
-
-            
-
-hero1 = hero(10,5,5,5)
-
-
-smallmppotion(hero1)
-largemppotion(hero1)
 hydrapotion(hero1)
